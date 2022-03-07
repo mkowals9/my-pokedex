87786f00820fb0f5b7641de98825dc0e69a49912
Marcelina Kowalska <01143915@pw.edu.pl>
2022-03-08 00:17:20 +0100

    getting every page almost correctly

--- src/components/MyPokemonList.js
+++ src/components/MyPokemonList.js
@@ -1,35 +1,29 @@
 import { connect } from "react-redux";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { mapToPokemon } from "../models/Pokemon";
 
 const MyPokemonList = (props) => {
 
     const [pokemonList, setPokemonList] = useState([])
+    const [currentUrl, setCurrentUrl] = useState(`https://pokeapi.co/api/v2/pokemon/`)
 
     async function* fetchPokemons() {
-        let url = `https://pokeapi.co/api/v2/pokemon/`;
-        while (url) {
-          const response = await fetch(url);
-          const body = await response.json();
-          let nextPage = response.headers.get('Link')
-          url = nextPage?.[1];
-      
-          for(let object of body.results) {
-            yield object;
-          }
+        let url = currentUrl;
+        const response = await fetch(url);
+        const body = await response.json();
+        url = body.next;
+        for(let object of body.results) {
+          yield object;
         }
+        setCurrentUrl(url)
       }
     
     const show = async () => {
-        let drugaLista = []
+        let baseData = []
         for await (const commit of fetchPokemons()) {
-            console.log("kuku ", commit)
-            drugaLista.push(commit)
+            baseData.push(commit)
         }
-
-        drugaLista.map(element => mapToPokemon(element))
-        console.log("druga lista ", drugaLista)
-        setPokemonList(drugaLista)    
+        setPokemonList(baseData)    
     }
 
     const getInfo = async () => {
@@ -38,9 +32,14 @@ const MyPokemonList = (props) => {
         console.log("lets see ", lista)
     }
 
+    const getAll = () => {
+      show()
+      getInfo()
+    }
+
     return(
         <>
-        <button onClick={() => show()}> Kliknij! </button>
+        <button onClick={() => getAll()}> Kliknij! </button>
         <button onClick={() => getInfo()}> Kliknij po 2! </button>
         </>
     )
--- src/models/Pokemon.js
+++ src/models/Pokemon.js
@@ -1,7 +1,6 @@
 export async function mapToPokemon(pokemon) {
     const response = await fetch(pokemon.url)
     const pokemonInfo = await response.json()
-    console.log("info ", pokemonInfo)
     return {
         name: pokemon.name,
         types: pokemonInfo.types.reduce(((types,type)=>[...types,type.type.name]),[]),
